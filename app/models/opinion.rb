78d66af43c8cb4d26ff505d30d38533ec812d2ce
EC2 Default User <ec2-user@ip-172-31-31-32.ap-northeast-1.compute.internal>
2022-08-05 06:26:13 +0000

    bootstrap

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,4 +2,8 @@ class Opinion < ApplicationRecord
 
   belongs_to :user
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, length: { minimum: 1, maximum: 200 }
+
 end
