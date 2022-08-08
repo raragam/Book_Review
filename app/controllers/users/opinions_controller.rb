0e2a4cb33dc1285e6affada0894ebae7918174e1
EC2 Default User <ec2-user@ip-172-31-29-135.ap-northeast-1.compute.internal>
2022-08-08 01:32:28 +0000

    8-8

--- app/controllers/users/opinions_controller.rb
+++ app/controllers/users/opinions_controller.rb
@@ -1,19 +1,55 @@
 class Users::OpinionsController < ApplicationController
   def index
+    @opinion = Opinion.new
+    @opinions = Opinion.all
+    @user = current_user
   end
 
   def create
+    @opinion = Opinion.new(opinion_params)
+    @opinion.user_id = current_user.id
+    if @opinion.save
+    flash[:notice] = "You have created opinion successfully."
+    redirect_to opinion_path(@opinion.id)
+    else
+    @opinions = Opinion.all
+    @user = current_user
+    render :index
+    end
   end
 
   def show
+    @opinion = Opinion.find(params[:id])
+    @user = @opinion.user
+    @opinion_new = Opinion.new
+    @opinions = Opinion.all
   end
 
   def edit
+    @opinion = Opinion.find(params[:id])
+    redirect_to opinions_path unless current_user.id == @opinion.user_id
   end
 
   def update
+    @opinion = Opinion.find(params[:id])
+    if @opinion.update(opinion_params)
+      flash[:notice] = "You have updated opinion successfully."
+      redirect_to opinion_path(@opinion.id)
+    else
+      @opinions = Opinion.all
+      render :edit
+    end
   end
 
   def destroy
+    @opinion = Opinion.find(params[:id])
+    @opinion.destroy
+    redirect_to '/opinions'
+  end
+
+  private
+
+  def opinion_params
+    params.require(:opinion).permit(:opinion_title, :opinion_body)
   end
 end
--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,16 +1,43 @@
 class Users::UsersController < ApplicationController
   def index
+    @opinion = Opinion.new
+    @user = User.new
+    @users = User.all
+    @user = current_user
   end
 
   def create
+    @opinion = Opinion.new(opinion_params)
+    @opinion.user_id = current_user.id
+    @opinion.save
+    redirect_to opinion_path(@opinion.id)
   end
 
   def show
+    @user = User.find(params[:id])
+    @user_my = current_user
+    @opinion_new = Opinion.new
+    @opinions = @user.opinions
   end
 
   def edit
+    @user = User.find(params[:id])
+    redirect_to user_path(current_user.id) unless @user == current_user
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image, :email, :password, :password_confirmation)
   end
 end
