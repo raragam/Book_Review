5fdbc00aba522d268137667f1a4d18eedf932b90
EC2 Default User <ec2-user@ip-172-31-29-135.ap-northeast-1.compute.internal>
2022-08-09 10:50:26 +0000

    19:50

--- app/controllers/users/reviews_controller.rb
+++ app/controllers/users/reviews_controller.rb
@@ -1,13 +1,32 @@
 class Users::ReviewsController < ApplicationController
   def new
+    @review = Review.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @review = Review.new(review_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @review.save
+    # 詳細画面へリダイレクト
+    redirect_to users_review_path(@review.id)
+
+    #render :new
+
   end
 
   def index
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
   end
 
-  def edit
+  private
+  # ストロングパラメータ
+  def review_params
+    params.require(:review).permit(:review_title, :review_body)
   end
+
 end
