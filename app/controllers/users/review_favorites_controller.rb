7bc31be49a89d81460eb5415b96c737cc8689183
EC2 Default User <ec2-user@ip-172-31-29-135.ap-northeast-1.compute.internal>
2022-09-18 13:55:17 +0000

    kaiketsu

---
+++ app/controllers/users/review_favorites_controller.rb
@@ -0,0 +1,28 @@
+class Users::ReviewFavoritesController < ApplicationController
+  def create
+    review = Review.find(params[:review_id])
+    review_favorite = current_user.review_favorites.new(review_id: review.id)
+    review_favorite.save
+    if params[:review][:up] == '1'
+       redirect_to users_reviews_path
+    elsif params[:review][:up] == '2'
+       redirect_to users_review_path(review)
+    else
+       redirect_to users_review_path(review)
+    end
+  end
+  def destroy
+    review = Review.find(params[:review_id])
+    review_favorite = current_user.review_favorites.find_by(review_id: review.id)
+    review_favorite.destroy
+    if params[:review][:down] == '1'
+       redirect_to users_reviews_path
+    elsif params[:review][:down] == '2'
+       redirect_to users_review_path(review)
+    elsif params[:review][:down] == '3'
+       redirect_to review_favorites_users_user_path(current_user.id)
+    else
+       redirect_to users_review_path(review)
+    end
+  end
+end
